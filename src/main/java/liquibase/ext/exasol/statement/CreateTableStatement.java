90c12a9190f3ad357397408b5526d74da37a0a15
Stuart Turton <Stuart.Turton@oracle-troubadour.co.uk>
2018-02-05 00:02:46 +0000

    liquibase-ext-exasol: correct syntax for RENAME column; add option for CREATE OR REPLACE TABLE

---
+++ src/main/java/liquibase/ext/exasol/statement/CreateTableStatement.java
@@ -0,0 +1,30 @@
+package liquibase.ext.exasol.statement;
+
+import liquibase.datatype.LiquibaseDataType;
+import liquibase.statement.*;
+
+import java.util.*;
+
+public class CreateTableStatement extends liquibase.statement.core.CreateTableStatement {
+
+    /**
+     * Exasol has a CREATE OR REPLACE option for most items.
+     **/
+    private boolean replaceIfExists = false;
+
+
+    public CreateTableStatement(String catalogName, String schemaName, String tableName) {
+        super(catalogName,schemaName,tableName);
+        this.replaceIfExists = false;
+    }
+
+    public CreateTableStatement(String catalogName, String schemaName, String tableName, boolean replaceIfExists) {
+       super(catalogName,schemaName,tableName);
+       this.replaceIfExists = replaceIfExists;
+    }
+
+    public boolean isReplaceIfExists() {
+        return replaceIfExists;
+    }
+
+}
