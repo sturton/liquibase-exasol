12f8aae24ca6f23758aa2871777f86302a4c69a7
Stuart Turton <Stuart.Turton@oracle-troubadour.co.uk>
2019-03-04 20:37:46 +0000

    3.5.6.1 version - Snapshot had 2 sets of columm entries. Replace inheritance by cut and paste and replaces()

--- src/main/java/liquibase/ext/exasol/snapshot/jvm/ColumnSnapshotGeneratorExasol.java
+++ src/main/java/liquibase/ext/exasol/snapshot/jvm/ColumnSnapshotGeneratorExasol.java
@@ -1,21 +1,26 @@
 package liquibase.ext.exasol.snapshot.jvm;
 
+import liquibase.database.AbstractJdbcDatabase;
 import liquibase.database.Database;
-import liquibase.database.core.OracleDatabase;
+import liquibase.ext.exasol.database.ExasolDatabase;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.DatabaseException;
 import liquibase.exception.UnexpectedLiquibaseException;
 import liquibase.ext.exasol.database.ExasolDatabase;
 import liquibase.logging.LogFactory;
 import liquibase.snapshot.CachedRow;
+import liquibase.snapshot.DatabaseSnapshot;
 import liquibase.snapshot.SnapshotGenerator;
 import liquibase.snapshot.jvm.ColumnSnapshotGenerator;
+import liquibase.snapshot.JdbcDatabaseSnapshot;
+import liquibase.snapshot.jvm.JdbcSnapshotGenerator;
 import liquibase.statement.DatabaseFunction;
 import liquibase.structure.DatabaseObject;
 import liquibase.structure.core.Column;
 import liquibase.structure.core.DataType;
 import liquibase.structure.core.Relation;
 import liquibase.structure.core.Table;
+import liquibase.structure.core.View;
 import liquibase.util.StringUtils;
 
 import java.sql.Connection;
@@ -23,22 +28,40 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import liquibase.database.OfflineConnection;
+import liquibase.executor.Executor;
+import liquibase.executor.ExecutorService;
+import liquibase.snapshot.InvalidExampleException;
+import liquibase.statement.core.RawSqlStatement;
+import liquibase.structure.core.Schema;
+import liquibase.util.SqlUtil;
 
-public class ColumnSnapshotGeneratorExasol extends ColumnSnapshotGenerator {
+public class ColumnSnapshotGeneratorExasol extends JdbcSnapshotGenerator {
+
+    private static final String LIQUIBASE_COMPLETE = "liquibase-complete";
 
 
     public ColumnSnapshotGeneratorExasol() {
-        super();
+	super(Column.class, new Class[]{Table.class, View.class});
     }
 
 
     @Override
     public int getPriority(Class<? extends DatabaseObject> objectType, Database database) {
-        return database instanceof ExasolDatabase ? SnapshotGenerator.PRIORITY_DATABASE : SnapshotGenerator.PRIORITY_NONE;
+        return database instanceof ExasolDatabase ? SnapshotGenerator.PRIORITY_ADDITIONAL : SnapshotGenerator.PRIORITY_NONE;
+    }
+
+    @Override
+    public Class<? extends SnapshotGenerator>[] replaces() {
+        Class<? extends SnapshotGenerator>[] replacedSnapshotGenerators  = ( Class<? extends SnapshotGenerator>[]) new Class[1] ;
+       	replacedSnapshotGenerators[0] = ColumnSnapshotGenerator.class ;
+        return replacedSnapshotGenerators ; 
     }
 
 
-    @Override
     protected Column readColumn(CachedRow columnMetadataResultSet, Relation table, Database database) throws SQLException, DatabaseException {
 
         String rawTableName = (String) columnMetadataResultSet.get("TABLE_NAME");
@@ -67,7 +90,7 @@ public class ColumnSnapshotGeneratorExasol extends ColumnSnapshotGenerator {
 
         if (database.supportsAutoIncrement()) {
             if (table instanceof Table) {
-                if (database instanceof OracleDatabase) {
+                if (database instanceof ExasolDatabase) {
                     String data_default = StringUtils.trimToEmpty((String) columnMetadataResultSet.get("DATA_DEFAULT")).toLowerCase();
                     if (data_default.contains("iseq$$") && data_default.endsWith("nextval")) {
                         column.setAutoIncrementInformation(new Column.AutoIncrementInformation());
@@ -135,5 +158,145 @@ public class ColumnSnapshotGeneratorExasol extends ColumnSnapshotGenerator {
 
         return column;
     }
+
+
+
+    @Override
+    protected DatabaseObject snapshotObject(DatabaseObject example, DatabaseSnapshot snapshot) throws DatabaseException, InvalidExampleException {
+        Database database = snapshot.getDatabase();
+        Relation relation = ((Column) example).getRelation();
+        if (((Column) example).getComputed() != null && ((Column) example).getComputed()) {
+            return example;
+        }
+        Schema schema = relation.getSchema();
+
+        List<CachedRow> columnMetadataRs = null;
+        try {
+
+            Column column = null;
+
+            if (example.getAttribute(LIQUIBASE_COMPLETE, false)) {
+                column = (Column) example;
+                example.setAttribute(LIQUIBASE_COMPLETE, null);
+            } else {
+                JdbcDatabaseSnapshot.CachingDatabaseMetaData databaseMetaData = ((JdbcDatabaseSnapshot) snapshot).getMetaData();
+
+                columnMetadataRs = databaseMetaData.getColumns(((AbstractJdbcDatabase) database).getJdbcCatalogName(schema), ((AbstractJdbcDatabase) database).getJdbcSchemaName(schema), relation.getName(), example.getName());
+
+                if (columnMetadataRs.size() > 0) {
+                    CachedRow data = columnMetadataRs.get(0);
+                    column = readColumn(data, relation, database);
+                    setAutoIncrementDetails(column, database, snapshot);
+                }
+            }
+
+            return column;
+        } catch (Exception e) {
+            throw new DatabaseException(e);
+        }
+    }
+
+    @Override
+    protected void addTo(DatabaseObject foundObject, DatabaseSnapshot snapshot) throws DatabaseException, InvalidExampleException {
+        if (!snapshot.getSnapshotControl().shouldInclude(Column.class)) {
+            return;
+        }
+        if (foundObject instanceof Relation) {
+            Database database = snapshot.getDatabase();
+            Relation relation = (Relation) foundObject;
+            List<CachedRow> allColumnsMetadataRs = null;
+            try {
+
+                JdbcDatabaseSnapshot.CachingDatabaseMetaData databaseMetaData = ((JdbcDatabaseSnapshot) snapshot).getMetaData();
+
+                Schema schema;
+
+                schema = relation.getSchema();
+                allColumnsMetadataRs = databaseMetaData.getColumns(((AbstractJdbcDatabase) database).getJdbcCatalogName(schema), ((AbstractJdbcDatabase) database).getJdbcSchemaName(schema), relation.getName(), null);
+
+                for (CachedRow row : allColumnsMetadataRs) {
+                    Column column = readColumn(row, relation, database);
+                    setAutoIncrementDetails(column, database, snapshot);
+                    column.setAttribute(LIQUIBASE_COMPLETE, true);
+                    relation.getColumns().add(column);
+                }
+            } catch (Exception e) {
+                throw new DatabaseException(e);
+            }
+        }
+
+    }
+
+    protected void setAutoIncrementDetails(Column column, Database database, DatabaseSnapshot snapshot) {
+        if (column.getAutoIncrementInformation() != null && database instanceof ExasolDatabase && database.getConnection() != null && !(database.getConnection() instanceof OfflineConnection)) {
+            Map<String, Column.AutoIncrementInformation> autoIncrementColumns = (Map) snapshot.getScratchData("autoIncrementColumns");
+            if (autoIncrementColumns == null) {
+                autoIncrementColumns = new HashMap<String, Column.AutoIncrementInformation>();
+                Executor executor = ExecutorService.getInstance().getExecutor(database);
+                try {
+                    List<Map<String, ?>> rows = executor.queryForList(new RawSqlStatement("select object_schema_name(object_id) as schema_name, object_name(object_id) as table_name, name as column_name, cast(seed_value as bigint) as start_value, cast(increment_value as bigint) as increment_by from sys.identity_columns"));
+                    for (Map row : rows) {
+                        String schemaName = (String) row.get("SCHEMA_NAME");
+                        String tableName = (String) row.get("TABLE_NAME");
+                        String columnName = (String) row.get("COLUMN_NAME");
+                        Long startValue = (Long) row.get("START_VALUE");
+                        Long incrementBy = (Long) row.get("INCREMENT_BY");
+
+                        Column.AutoIncrementInformation info = new Column.AutoIncrementInformation(startValue, incrementBy);
+                        autoIncrementColumns.put(schemaName+"."+tableName+"."+columnName, info);
+                    }
+                    snapshot.setScratchData("autoIncrementColumns", autoIncrementColumns);
+                } catch (DatabaseException e) {
+                    LogFactory.getInstance().getLog().info("Could not read identity information", e);
+                }
+            }
+            if (column.getRelation() != null && column.getSchema() != null) {
+                Column.AutoIncrementInformation autoIncrementInformation = autoIncrementColumns.get(column.getSchema().getName() + "." + column.getRelation().getName() + "." + column.getName());
+                if (autoIncrementInformation != null) {
+                    column.setAutoIncrementInformation(autoIncrementInformation);
+                }
+            }
+        }
+    }
+
+    protected DataType readDataType(CachedRow columnMetadataResultSet, Column column, Database database) throws SQLException {
+
+        String columnTypeName = (String) columnMetadataResultSet.get("TYPE_NAME");
+
+
+        DataType.ColumnSizeUnit columnSizeUnit = DataType.ColumnSizeUnit.BYTE;
+
+        int dataType = columnMetadataResultSet.getInt("DATA_TYPE");
+        Integer columnSize = null;
+        Integer decimalDigits = null;
+        if (!database.dataTypeIsNotModifiable(columnTypeName)) { // don't set size for types like int4, int8 etc
+            columnSize = columnMetadataResultSet.getInt("COLUMN_SIZE");
+            decimalDigits = columnMetadataResultSet.getInt("DECIMAL_DIGITS");
+            if (decimalDigits != null && decimalDigits.equals(0)) {
+                decimalDigits = null;
+            }
+        }
+
+        Integer radix = columnMetadataResultSet.getInt("NUM_PREC_RADIX");
+
+        Integer characterOctetLength = columnMetadataResultSet.getInt("CHAR_OCTET_LENGTH");
+
+
+        DataType type = new DataType(columnTypeName);
+        type.setDataTypeId(dataType);
+        type.setColumnSize(columnSize);
+        type.setDecimalDigits(decimalDigits);
+        type.setRadix(radix);
+        type.setCharacterOctetLength(characterOctetLength);
+        type.setColumnSizeUnit(columnSizeUnit);
+
+        return type;
+    }
+
+    protected Object readDefaultValue(CachedRow columnMetadataResultSet, Column columnInfo, Database database) throws SQLException, DatabaseException {
+
+        return SqlUtil.parseValue(database, columnMetadataResultSet.get("COLUMN_DEF"), columnInfo.getType());
+    }
+
 }
 
